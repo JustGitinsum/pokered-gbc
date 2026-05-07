7406d0580737639f1bf57af013c94e2c6ec0a218
BuildTools <unconfigured@null.spigotmc.org>
2026-05-06 21:16:26 -0500

    Pressing Select button in the Party Menu swaps pokemon
    Signed-off-by: BuildTools <unconfigured@null.spigotmc.org>

--- home/pokemon.asm
+++ home/pokemon.asm
@@ -224,14 +224,18 @@ PartyMenuInit::
 ; otherwise, it is 0
 .storeMaxMenuItemID
 	ld [hli], a ; max menu item ID
-	ld a, [wForcePlayerToChooseMon]
-	and a
-	ld a, PAD_A | PAD_B
-	jr z, .next
-	xor a
-	ld [wForcePlayerToChooseMon], a
-	inc a ; a = PAD_A
+	; ld a, [wForcePlayerToChooseMon]
+	; and a
+	; ld a, PAD_A | PAD_B
+	; jr z, .next
+	; xor a
+	; ld [wForcePlayerToChooseMon], a
+	; inc a ; a = PAD_A
+	push hl
+	callfar GetPartyMenuWatchedKeys
+	pop hl
 .next
+	ld a, d
 	ld [hli], a ; menu watched keys
 	pop af
 	ld [hl], a ; old menu item ID
@@ -249,6 +253,21 @@ HandlePartyMenuInput::
 	ld [wPartyMenuAnimMonEnabled], a
 	ld a, [wCurrentMenuItem]
 	ld [wPartyAndBillsPCSavedMenuItem], a
+	bit B_PAD_SELECT, b
+	jr z, .notSelect
+	push af
+	ld a, SFX_PRESS_AB
+	call PlaySound
+	ld a, [wMenuItemToSwap]
+	and a
+	jr nz, .swap
+	pop af
+	inc a ; [wMenuItemToSwap] counts from 1
+	ld [wMenuItemToSwap], a
+	jr HandlePartyMenuInput
+.swap
+	pop af
+.notSelect
 	ld hl, wStatusFlags5
 	res BIT_NO_TEXT_DELAY, [hl]
 	ld a, [wMenuItemToSwap]
@@ -286,12 +305,12 @@ HandlePartyMenuInput::
 	ld [wMenuItemToSwap], a
 	ld [wPartyMenuTypeOrMessageID], a
 	call RedrawPartyMenu
-	jr HandlePartyMenuInput
+	jp HandlePartyMenuInput
 .handleSwap
 	ld a, [wCurrentMenuItem]
 	ld [wWhichPokemon], a
 	farcall SwitchPartyMon
-	jr HandlePartyMenuInput
+	jp HandlePartyMenuInput
 
 DrawPartyMenu::
 	ld hl, DrawPartyMenu_
