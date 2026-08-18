6b782599d8613448ed784553860ed1a56eb60a8d
BuildTools <unconfigured@null.spigotmc.org>
2026-08-18 14:57:49 -0500

    Changes made to overworld tileset, palettes, and some maps
    Signed-off-by: BuildTools <unconfigured@null.spigotmc.org>

--- data/maps/objects/FuchsiaCity.asm
+++ data/maps/objects/FuchsiaCity.asm
@@ -15,11 +15,11 @@ FuchsiaCity_Object:
 
 	def_warp_events
 	warp_event  5, 13, FUCHSIA_MART, 1
-	warp_event 11, 27, FUCHSIA_BILLS_GRANDPAS_HOUSE, 1
+	warp_event 13, 27, FUCHSIA_BILLS_GRANDPAS_HOUSE, 1
 	warp_event 19, 27, FUCHSIA_POKECENTER, 1
 	warp_event 27, 27, WARDENS_HOUSE, 1
 	warp_event 18,  3, SAFARI_ZONE_GATE, 1
-	warp_event  5, 27, FUCHSIA_GYM, 1
+	warp_event  7, 27, FUCHSIA_GYM, 1
 	warp_event 22, 13, FUCHSIA_MEETING_ROOM, 1
 	warp_event 31, 27, FUCHSIA_GOOD_ROD_HOUSE, 2
 	warp_event 31, 24, FUCHSIA_GOOD_ROD_HOUSE, 1
